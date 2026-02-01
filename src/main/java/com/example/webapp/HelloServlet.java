12a207a8b743ae922bcd2f1b212c8f7378ba1328
정소영 <so2043@naver.com>
2026-02-02 07:13:49 +0900

    Initial commit: Maven WAR project

---
+++ src/main/java/com/example/webapp/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.example.webapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+            throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Hello from WAR deployment!</h1>");
+        out.println("<p>Version: 1.0</p>");
+        out.println("<p>Jenkins CI/CD Test</p>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>WAR Deployment Test</title>
+</head>
+<body>
+    <h1>Welcome!</h1>
+    <p>This is a WAR deployment test.</p>
+    <a href="hello">Go to Servlet</a>
+</body>
+</html>
